ec3f62e49fdc2b49ab74596715deb50aed5b5935
oriana <oriana.nahir.r@gmail.com>
2024-01-11 16:47:54 +0100

    menuButton done

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import './MenuButton.css';
+import { useNavigate } from "react-router-dom";
 
-const MenuButton = ({title, subtitle, buttonMsg}) => {
+const MenuButton = ({title, subtitle, route}) => {
+  const navigate = useNavigate();
+
+  const goToMenu = () => {
+    navigate(`/${route}`)
+  }
+
   return (
-    <div>MenuButton</div>
+    <div className='button-container'>
+        <h1 className='button__title'>{title}</h1>
+        <p className='button__subtitle'>{subtitle}</p>
+        <button onClick={goToMenu} className='button__btn'>Go  <ChevronRightIcon sx={{fontSize: 'medium'}}/></button>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import landing2 from '/images/landing2.jpg';
 import landing3 from '/images/landing3.jpg';
 import LandingImg from '../../components/LandingImg/LandingImg';
 import Slider from '../../components/Slider/Slider';
+import MenuButton from '../../components/MenuButton/MenuButton';
 
 const Home = () => {
 
@@ -16,6 +17,9 @@ const Home = () => {
         <Slider imageSrc={landing1} title={'Neon Notes: A Symphony in Spirits'} subtitle={'Where Vinyl Spins and the Night Begins'} />
         <Slider imageSrc={landing2} title={'Harmony on the Wall: A Visual Concerto'} subtitle={'Where Art, Music, and Inspiration Collide'} flipped={true} />
         <Slider imageSrc={landing3} title={'Jukebox Jamboree: A Melody of Memories'} subtitle={'Step into the Spotlight of the Rock Records Restaurant'} />
+      </div>
+      <div className='menu-btn_container'>
+        <MenuButton title={'Rock Your Taste Buds!'} subtitle={'Dive into our menu of rockin’ burgers. Hit the button below and let the feast begin!'} route={'menu'}/>
       </div>
       
     </div>
