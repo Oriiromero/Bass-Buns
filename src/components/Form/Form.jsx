f19a0d826339b5ceae7db6502193b9241c3ff340
oriana <oriana.nahir.r@gmail.com>
2024-10-26 15:04:50 +0200

    contact page done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Grid, Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { useForm } from "react-hook-form";
 import './Form.css';
@@ -7,7 +7,7 @@ import './Form.css';
 const Form = () => {
 
     const [formData, setFormData] = useState();
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const [loading, setLoading] = useState(false);
     const [buttonText, setButtonText] = useState('Send');
 
@@ -26,8 +26,9 @@ const Form = () => {
         setFormData(obj);
 
         setTimeout(() => {
-            setLoading(false)
-        }, 6000)
+            setLoading(false),
+            reset();
+        }, 4000)
 
 
         setButtonText('Sent ✔');
@@ -35,69 +36,66 @@ const Form = () => {
     }
 
     return (
-        <Grid>
-            <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto", backgroundColor: "transparent" }} className='contact-form'>
-                <CardContent sx={{ width: "100%" }}>
-                    <Typography gutterBottom style={{ color: "#FF9F29", fontFamily: "'Bebas Neue', sans-serif", fontSize: "25px" }}>
-                        Contact Us!
-                    </Typography>
-                    <Typography variant="body2" gutterBottom style={{ color: "#EEEEEE", fontFamily: "'Bebas Neue', sans-serif", fontSize: "20px" }}>
-                        Fill up the form and our team will get back to you within 24 hours.
-                    </Typography>
-                    <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto", backgroundColor: "transparent" }}>
-                        <form className="contact-main_form" onSubmit={handleSubmit(onSubmit)}>
-                            <label className='form-label' >Name</label>
-                            <input
-                                className="contact-main_form__input"
-                                type="text"
-                                placeholder="Your name..."
-                                {...register("name", { required: true })}
-                            />
-                            {errors.name?.type === 'required' && <span className="error-message">Name is required</span>}
-                            <label className='form-label'>Surname</label>
-                            <input
-                                className="contact-main_form__input"
-                                type="text"
-                                placeholder="Your surname..."
-                                {...register("surname", { required: true })}
-                            />
-                            {errors.surname?.type === 'required' && <span className="error-message">Surname is required</span>}
-                            <label className='form-label'>Email</label>
-                            <input
-                                className="contact-main_form__input"
-                                type="email"
-                                placeholder="Your email..."
-                                {...register("email", { required: true })}
-                            />
-                            {errors.email?.type === 'required' && <span className="error-message">Email is required</span>}
-                            <label className='form-label'>Phone</label>
-                            <input
-                                className="contact-main_form__input"
-                                type="number"
-                                placeholder="Your phone..."
-                                {...register("phone", { required: false })}
-                            />
-                            <label className='form-label'>Message</label>
-                            <textarea
-                                className="contact-main_form__textarea"
-                                placeholder="Your message..."
-                                {...register("message", { required: true })}
-                                maxLength={200}
-                            />
-                            {errors.message?.type === 'required' && <span className="error-message">Message is required</span>}
-                            <button className="contact-main_form__btn">
-                                {!loading ?
-                                    buttonText
-                                    :
-                                    <LoadingButton loading variant="text" size="medium" style={{color: "white"}}/>
-                                }
-                            </button>
-                        </form>
-                    </Card>
-
-                </CardContent>
-            </Card>
-        </Grid>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto", backgroundColor: "transparent", boxShadow: 'none' }} className='contact-form'>
+            <CardContent sx={{ width: "100%" }}>
+                <Typography gutterBottom style={{ color: "#FF9F29", fontFamily: "'Bebas Neue', sans-serif", fontSize: "25px" }}>
+                    Contact Us!
+                </Typography>
+                <Typography variant="body2" gutterBottom style={{ color: "#EEEEEE", fontFamily: "'Bebas Neue', sans-serif", fontSize: "20px" }}>
+                    Fill up the form and our team will get back to you within 24 hours.
+                </Typography>
+                <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto", backgroundColor: "transparent", boxShadow: 'none' }}>
+                    <form className="contact-main_form" onSubmit={handleSubmit(onSubmit)}>
+                        <label className='form-label' >Name</label>
+                        <input
+                            className="contact-main_form__input"
+                            type="text"
+                            placeholder="Your name..."
+                            {...register("name", { required: true })}
+                        />
+                        {errors.name?.type === 'required' && <span className="error-message">Name is required</span>}
+                        <label className='form-label'>Surname</label>
+                        <input
+                            className="contact-main_form__input"
+                            type="text"
+                            placeholder="Your surname..."
+                            {...register("surname", { required: true })}
+                        />
+                        {errors.surname?.type === 'required' && <span className="error-message">Surname is required</span>}
+                        <label className='form-label'>Email</label>
+                        <input
+                            className="contact-main_form__input"
+                            type="email"
+                            placeholder="Your email..."
+                            {...register("email", { required: true })}
+                        />
+                        {errors.email?.type === 'required' && <span className="error-message">Email is required</span>}
+                        <label className='form-label'>Phone</label>
+                        <input
+                            className="contact-main_form__input"
+                            type="number"
+                            placeholder="Your phone..."
+                            {...register("phone", { required: false })}
+                        />
+                        <label className='form-label'>Message</label>
+                        <textarea
+                            className="contact-main_form__textarea"
+                            placeholder="Your message..."
+                            {...register("message", { required: true })}
+                            maxLength={200}
+                        />
+                        {errors.message?.type === 'required' && <span className="error-message">Message is required</span>}
+                        <button className="contact-main_form__btn">
+                            {!loading ?
+                                buttonText
+                                :
+                                <LoadingButton loading variant="text" size="medium" style={{ color: "white" }} />
+                            }
+                        </button>
+                    </form>
+                </Card>
+            </CardContent>
+        </Card>
     )
 }
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -10,18 +10,18 @@ import { Card, CardContent, Typography } from '@mui/material';
 const Contact = () => {
   return (
     <>
-      <Container className='contact-container' sx={{display: 'flex', alignItems: 'center', width: '100%', height: '100vh'}}>
-        <Box sx={{ width: '50%' }}>
+      <Container className='contact-container' sx={{display: 'flex', alignItems: 'center', width: '100%', height: '100%'}}>
+        <Box sx={{ width: '100%' }}>
           {/* informacion de direccion telefono etc */}
-          <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <CardContent>
-              <h4>Want to contact us?</h4>
-              <p>You can reach us at:</p>
+          <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'transparent', boxShadow: 'none'}}>
+            <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <h4 className='contact-title'>Want to contact us?</h4>
+              <p className='contact-subtitle'>You can reach us at:</p>
             </CardContent>
-            <CardContent>
-              <p>Fake address 123, Country</p>
-              <p>000 111 222 333</p>
-              <p>bass_buns@email.com</p>
+            <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <p className='personal-directions'>Fake address 123, Country</p>
+              <p className='personal-phone'>000 111 222 333</p>
+              <p className='personal-email'>bass_buns@email.com</p>
             </CardContent>
           </Card>
         </Box>
